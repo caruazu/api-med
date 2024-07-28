dee3d54633ff2644694447767fc8d55b3303136a
caruazu <gustavocaruazu@gmail.com>
2024-07-28 18:37:24 -0300

    testando um controller

--- src/main/java/med/caruazu/api/ApiApplication.java
+++ src/main/java/med/caruazu/api/ApiApplication.java
@@ -1,18 +1,12 @@
 package med.caruazu.api;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication implements CommandLineRunner {
+public class ApiApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Vamos construir noossa API");
 	}
 }
