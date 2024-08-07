5fc1adc9fc4a92e07edb4344c898c86f8cdd3abe
caruazu <gustavocaruazu@gmail.com>
2024-08-07 17:45:10 -0300

    controle de acesso

--- src/main/java/med/caruazu/api/infra/security/TokenService.java
+++ src/main/java/med/caruazu/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.caruazu.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.caruazu.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,21 @@ public class TokenService {
 					.sign(algoritimo);
 		} catch (JWTCreationException exception){
 			throw new RuntimeException("erro ao gerar o token JWT", exception);
+		}
+	}
+
+	public String decodeToken(String tokenJWT) {
+		try {
+			Algorithm algoritimo = Algorithm.HMAC256(secret);
+			return JWT.require(algoritimo)
+					// specify any specific claim validations
+					.withIssuer("api-med")
+					// reusable verifier instance
+					.build()
+					.verify(tokenJWT)
+					.getSubject();
+		} catch (JWTVerificationException exception){
+			throw new RuntimeException("token inválido", exception);
 		}
 	}
 
